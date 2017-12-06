4c57cf5ab4e3b82d859dbc34d7f561da1e3f66bb
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-07 04:29:27 +0530

    Update master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,6 +1,23 @@
 #install jenkins as master
 
-class { 'jenkins':
-  slaveagentport => 0 + $::slaveport,
+include jenkins
+
+#run custom groovy script
+$scriptdata = hiera('customscript')
+
+$scriptdata.each |$item| {
+
+  $input = $item[data]
+  $script = $item[script]
+
+ file { "/tmp/$script.groovy":
+   ensure  => present,
+   content => template("master/$script.groovy"),
+   before  => Exec["$script"]
   }
-include jenkins::security
+  exec { "$script":
+    path      => ['/usr/bin','usr/local/bin','/bin'],
+    command   => "curl -d \"script=\$(cat /tmp/$script.groovy)\"  http://localhost:8080/scriptText",
+    logoutput => true
+   }
+}

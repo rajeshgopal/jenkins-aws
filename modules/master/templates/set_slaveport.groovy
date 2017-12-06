12849db790539a8a65af66de5e17f9fffce2d628
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-07 04:47:40 +0530

    Create set_slaveport.groovy

---
+++ modules/master/templates/set_slaveport.groovy
@@ -0,0 +1,17 @@
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import groovy.transform.InheritConstructors
+import hudson.model.*
+import hudson.plugins.sshslaves.*
+import hudson.util.*
+import jenkins.model.*
+import jenkins.security.*
+import org.apache.commons.io.IOUtils
+import org.jenkinsci.plugins.*
+
+def j = Jenkins.getInstance()
+    j.setSlaveAgentPort(<%= @input['slaveport'] %>)
+    j.save()

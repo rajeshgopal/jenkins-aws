19e4079338f4a071b7f36ae016daf95afd051736
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-07 04:52:08 +0530

    Create set_security.groovy

---
+++ modules/master/templates/set_security.groovy
@@ -0,0 +1,38 @@
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import groovy.transform.InheritConstructors
+import hudson.model.*
+import hudson.plugins.sshslaves.*
+import hudson.util.*
+import jenkins.model.*
+import jenkins.security.*
+import org.apache.commons.io.IOUtils
+import org.jenkinsci.plugins.*
+
+def j = Jenkins.getInstance()
+def security_model = "<%= @input['model'] %>"
+    if (security_model == 'disabled') {
+      j.disableSecurity()
+      return null
+    }
+
+    def strategy
+    def realm
+    switch (security_model) {
+      case 'fullcontrol':
+        strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+        realm = new hudson.security.HudsonPrivateSecurityRealm(false, false, null)
+        break
+      case 'unsecured':
+        strategy = new hudson.security.AuthorizationStrategy.Unsecured()
+        realm = new hudson.security.HudsonPrivateSecurityRealm(false, false, null)
+        break
+      default:
+        throw new InvalidAuthenticationStrategy()
+    }
+    j.setAuthorizationStrategy(strategy)
+    j.setSecurityRealm(realm)
+    j.save()

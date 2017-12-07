0a6ecb86aec60dc76ae7d34d12fc33fb8d4d164d
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-07 06:24:42 +0530

    Update master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -20,6 +20,7 @@ $scriptdata.each |$item| {
     command   => "curl -d \"script=\$(cat /tmp/$script.groovy)\"  http://localhost:8080/scriptText",
     logoutput => true,
     onlyif    => "until 'curl -s -o /dev/null -I http://localhost:8080/' ; do sleep 10  done",
+    provider  => bash,
     require   => Service['jenkins']
    }
 }

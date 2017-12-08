89620908198e4962bb16b9224598783addde21bb
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-09 01:01:40 +0530

    Update master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -2,33 +2,11 @@
 
 include jenkins
 
-#run custom groovy script
-$scriptdata = lookup('customscript')
+include master
 
-exec { 'sleep120':
-  command => 'sleep 120',
+exec { 'waitonmaster':
+  command => 'sleep 180',
   path    => ['/bin'],
-  require => Service['jenkins'],
-  before  => Exec["$script"],
 }
 
-$scriptdata.each |$item| {
-
-  $input = $item[data]
-  $script = $item[script]
-
-  file { "/tmp/$script.groovy":
-    ensure  => present,
-    content => template("master/$script.groovy"),
-    before  => Exec["$script"]
-  }
-  exec { "$script":
-    path      => ['/usr/bin','usr/local/bin','/bin'],
-    command   => "curl -d \"script=\$(cat /tmp/$script.groovy)\"  http://localhost:8080/scriptText",
-    logoutput => true,
-    onlyif    => "/usr/bin/wget --spider --tries 30 --retry-connrefused http://localhost:8080/",
-    provider  => 'shell',
-    require   => Service['jenkins'],
-    notify    => Service['jenkins']
-  }
-}
+Class['jenkins'] -> Exec['waitonmaster'] -> Class['master']

15c76fa52305a286ec2ecf01c3b4f26031c72056
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-09 01:04:48 +0530

    Create init.pp

---
+++ modules/master/manifests/init.pp
@@ -0,0 +1,54 @@
+#class for configuring jenkins using groovy scripts
+## Example Hiera Data
+#master::settings:
+#  user:
+#    script: add_user
+#    data:
+#      - user: admin
+#        passwd: "%{::masterpwd}"
+#      - user: "%{::slaveuser}"
+#        passwd: "%{::slavepwd}"
+#  executor:
+#    script: set_executors
+#    data:
+#      count: 0
+#  agent:
+#    script: set_slaveport
+#    data:
+#      slaveport: "%{::slaveport}"
+#  security:
+#    script: set_security
+#    data:
+#      model: fullcontrol
+
+class master(
+  Optional[Hash] $settings = undef,
+  String $host = "http://localhost:8080"
+ ){
+
+ if !empty($settings) {
+
+   package { 'wget':
+     ensure => installed,
+   }
+
+   $settings.each |$key, $item| {
+
+     $input = $item[data]
+     $script = $item[script]
+
+     file { "/tmp/$script.groovy":
+       ensure  => present,
+       content => template("master/$script.groovy"),
+       before  => Exec["$script"]
+     }
+     exec { "$script":
+       path      => ['/usr/bin','usr/local/bin','/bin'],
+       command   => "curl -d \"script=\$(cat /tmp/$script.groovy)\"  ${host}/scriptText",
+       logoutput => true,
+       onlyif    => "/usr/bin/wget --spider --tries 30 --retry-connrefused ${host}",
+       provider  => 'shell',
+     }
+   }
+ }
+}

5fd68cbc9d0057fff8252f8b182dae8d851ba678
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-16 00:47:29 +0530

    Create global_tool.groovy

---
+++ modules/master/templates/global_tool.groovy
@@ -0,0 +1,19 @@
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import groovy.transform.InheritConstructors
+import hudson.model.*
+import hudson.plugins.sshslaves.*
+import hudson.util.*
+import jenkins.model.*
+import jenkins.security.*
+import org.apache.commons.io.IOUtils
+import org.jenkinsci.plugins.*
+    
+    def inst = Jenkins.getInstance()    
+    def desc = inst.getDescriptor("hudson.tasks.Maven")
+    def minst =  new hudson.tasks.Maven.MavenInstallation("<%= @input['name'] %>", "<%= @input['mavenpath'] %>");
+    desc.setInstallations(minst)
+    desc.save()

dc66019b4f52e11b51c764798176dd066bf6ba21
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-16 00:42:00 +0530

    Update slave.pp

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -3,9 +3,16 @@
 include jenkins::slave
 
 $list = lookup('packageslist')
+$mavenarchive = lookup( { "name" => "mavenarchive", "default_value" => "http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz" } )
+
 $list.each |$item| {
   package { "$item":
     ensure => installed,
   }
 }
-
+exec { 'maven-install-from-archive':
+  path    => ['/bin', '/usr/bin'],
+  command => "tar -xf $mavenarchive -C /opt/maven --strip-components=1",
+  cwd     => '/opt',
+  onlyif  => "mkdir maven",
+}

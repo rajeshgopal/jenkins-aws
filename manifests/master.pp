00959fd904d3cf5c029befb5a369080bc2e5e096
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-21 21:23:16 +0530

    Update master.pp

--- manifests/master.pp
+++ manifests/master.pp
@@ -14,5 +14,10 @@ exec { 'addjob':
   path    => ['/usr/bin', '/bin'],
   cwd     => '/opt/garage/modules/master/files',
 }
+exec { 'addjob':
+  command => "curl -s -XPOST 'http://localhost:8080/createItem?name=docker-build-pipeline-job' --data-binary @docker-build-pipeline.xml -H 'Content-Type:text/xml' ",
+  path    => ['/usr/bin', '/bin'],
+  cwd     => '/opt/garage/modules/master/files',
+}
 
 Class['jenkins'] -> Exec['waitonmaster'] -> Exec['addjob'] -> Class['master']

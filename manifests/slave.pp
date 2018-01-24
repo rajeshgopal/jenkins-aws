4caae6224422aec52e54d4fbca0cc862437de86b
rajeshgopal <rajeshsmyl@gmail.com>
2018-01-24 19:17:45 +0530

    Update slave.pp

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -4,19 +4,31 @@ include jenkins::slave
 
 $list = lookup('packageslist')
 $mavenarchive = lookup( { "name" => "mavenarchive", "default_value" => "http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz" } )
-$filename = "apache-maven-3.5.2-bin.tar.gz"
+$terraformzip = lookup( { "name" => "terraformzip", "default_value" => "https://releases.hashicorp.com/terraform/0.11.2/terraform_0.11.2_linux_amd64.zip" } )
 
 $list.each |$item| {
   package { "$item":
     ensure => installed,
   }
 }
+if $terraformzip =~ /(.*\/)(.*\.*)/ {
+  archive { "/tmp/$2":
+    source       => "$terraformzip",
+    extract      => true,
+    extract_path => '/usr/local/bin',
+    creates      => '/usr/local/bin/terraform',
+    user         => 'jenkins-slave',
+    group        => 'jenkins-slave',
+    cleanup      => true,
+  }
+}
 
-exec { 'maven-install-from-archive':
-  path    => ['/bin', '/usr/bin'],
-  command => "tar -xf $filename -C /opt/maven --strip-components=1",
-  cwd     => '/opt',
-  onlyif  => "mkdir maven && wget $mavenarchive",
+if $mavenarchive =~ /(.*\/)(.*\.*)/ {
+  exec { 'maven-install-from-archive':
+    path    => ['/bin', '/usr/bin'],
+    command => "tar -xf $2 -C /opt/maven --strip-components=1",
+    cwd     => '/opt',
+    onlyif  => "mkdir maven && wget $mavenarchive",
 }
 
 yumrepo { 'docker-repo':
